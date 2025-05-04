3032a8df1ab623b9a458fba12a13a44524ce80ba
edalal <edalal@umich.edu>
2025-05-04 18:13:28 -0400

    fix: cleaned up extra files and resolved merge conflicts

--- arduino_data/python/config.py
+++ arduino_data/python/config.py
@@ -1,4 +1,5 @@
 # config.py
+<<<<<<< Updated upstream
 SERIAL_PORT = '/dev/cu.usbmodem744DBD9FD0942'  # ← change to your port (check Arduino IDE)
 BAUD_RATE = 115200 # don't touch
 SAMPLING_RATE = 50  # Hz
@@ -9,3 +10,11 @@ DURATION = 10 # trial length in seconds, set to None for continuous logging
 
 #TODO: add script to run log data and prompt label and duration
 #TODO: add requirements.txt for conda environment
+=======
+# SERIAL_PORT = '/dev/cu.usbmodem744DBD9FD0942'  # ← change to your port
+SERIAL_PORT = '/dev/cu.usbmodem744DBDA09AAC2'
+BAUD_RATE = 115200
+SAMPLING_RATE = 50  # Hz
+LABEL = 'stressed'  # ← you can make this dynamic later
+DURATION = None # seconds, set to None for continuous logging
+>>>>>>> Stashed changes
